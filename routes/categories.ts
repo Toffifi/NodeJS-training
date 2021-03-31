e59bd81d6ee13a30ee82d7aa8e510d2fb1d1e083
Toffifi <sanyq@mail.ru>
2021-03-31 14:36:53 +0300

    fix: typo

--- routes/categories.ts
+++ routes/categories.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 
-import { create, get, getAll, remove, update } from '../controllers/categories';
+import { create, get, getAll } from '../controllers/categories';
 
 const createCategoryRoutes = (router: Router) => {
   router.get('/categories', getAll);
   router.get('/categories/:id', get);
   router.post('/categories', create);
-  router.put('/categories/:id', update);
-  router.delete('/categories/:id', remove);
+  // router.put('/categories/:id', update);
+  // router.delete('/categories/:id', remove);
 };
 
 export default createCategoryRoutes;
